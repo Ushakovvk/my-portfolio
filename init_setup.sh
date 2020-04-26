378df8df152d4cc08cfd5d0418a991a0d0d132e4
Vadim Ushakov <ushakovvk@gmail.com>
2020-04-26 17:55:44 +0300

    first commit

---
+++ init_setup.sh
@@ -0,0 +1,3 @@
+git clone -b master https://github.com/Ushakovvk/optimal_rent.git
+git clone -b app https://github.com/Ushakovvk/capstone-sentiment-analysis.git
+git clone -b apps https://github.com/Ushakovvk/capstone-taxi.git
