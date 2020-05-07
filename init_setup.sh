eed49cb158c22bea030879dbaa2a32b22c05928e
Vadim Ushakov <ushakovvk@gmail.com>
2020-05-08 01:32:50 +0300

    удалил старые приложения добавил docker-compose

--- init_setup.sh
+++ init_setup.sh
@@ -1,3 +1,21 @@
-git clone -b master https://github.com/Ushakovvk/optimal_rent.git
-git clone -b app https://github.com/Ushakovvk/capstone-sentiment-analysis.git
-git clone -b apps https://github.com/Ushakovvk/capstone-taxi.git
+# install docker
+#curl -fsSL https://get.docker.com -o get-docker.sh
+#sh get-docker.sh
+#sudo groupadd docker
+#sudo usermod -aG docker $USER
+#newgrp docker
+
+# install docker-compose
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
+
+# download apps
+#git clone -b master https://github.com/Ushakovvk/my-portfolio.git
+#git clone -b master https://github.com/Ushakovvk/optimal-rent.git
+#git clone -b app https://github.com/Ushakovvk/capstone-sentiment-analysis.git
+#git clone -b apps https://github.com/Ushakovvk/capstone-taxi.git
+
+# build and start apps
+cd ./my-portfolio/
+docker-compose build
+docker stack deploy -c docker-compose.yml portfolio
